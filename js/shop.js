e46e931f27b8e5a708a746823414c2844e5d7906
j-choriki <102720007+j-choriki@users.noreply.github.com>
2023-12-09 13:54:10 +0900

    shopデザイン

--- js/shop.js
+++ js/shop.js
@@ -23,39 +23,79 @@ document.addEventListener("DOMContentLoaded", function() {
             // console.log(xmlDoc);
             // 表示したい項目をxmlDocから取り出し
             const shopElements = xmlDoc.querySelectorAll('shop')[0]; 
-            
-            //表示情報取得部分
-            const name = shopElements.querySelector('name').textContent;            //店舗名
+            console.log(shopElements);
+
+            //XMLから表示情報取得部分
+            const logoImage = shopElements.querySelector('logo_image').textContent; //レストランロゴ
+            const name = shopElements.querySelector('name').textContent;            //レストラン名
+            const photoUrl = shopElements.querySelector('photo').querySelector('pc').querySelector('m').textContent;       //表示画像URL        
             const address = shopElements.querySelector('address').textContent;      //住所
             const serviceTime = shopElements.querySelector('open').textContent;     //営業時間
+            const closeTime = shopElements.querySelector('close').textContent;     //営業時間
+            const card = shopElements.querySelector('card').textContent;           //カードの利用可否
+            const partyCapa = shopElements.querySelector('party_capacity').textContent; //最大人数
+            const smoking = shopElements.querySelector('non_smoking').textContent;
+            const parking = shopElements.querySelector('parking').textContent;
             const shopUrl = shopElements.querySelector('urls').querySelector('pc').textContent; //お店webサイトのwebサイトのURL
+            const mapUrl = shopElements.querySelector('coupon_urls').querySelector('pc').textContent; //Hot Pepperの地図情報掲載ページへのURL
 
-            const shopParam = [name, address, serviceTime,shopUrl];
+          //======ロゴとレストラン名======================================
+            const h3 = document.createElement('h3');
+            h3.textContent = name;
+            let img = document.createElement('img');
+            img.src= logoImage;
+            const div_header = document.createElement('div');
+            div_header.className = "div_header";
+            div_header.appendChild(img);
+            div_header.appendChild(h3);
+            section.appendChild(div_header);
 
-            //表示画像の生成
-            const photoUrl = shopElements.querySelector('photo').querySelector('pc').querySelector('l').textContent;       //表示画像URL
-            const img = document.createElement('img');
+
+            //======レストラン画像、住所、営業時間を表示======================================
+            const div_main = document.createElement('div');
+            div_main.className = "div_main";
+
+            // レストラン画像の生成
+            img = document.createElement('img');
             img.src = photoUrl
-            const photo = document.createElement('div');
-            photo.appendChild(img);
-            section.appendChild(photo); 
+            const div_photo = document.createElement('div');
+            div_photo.appendChild(img);
+            div_main.appendChild(div_photo); 
 
-            //pタグを生成して表示項目を追加していく
+
+            //ロゴとレストラン名を以外をtableで表示
+            const table = document.createElement('table');
+
+            //以下for文で使うための配列
+            const shopParam = [address, mapUrl, serviceTime,closeTime, card, partyCapa, smoking, parking,shopUrl];
+            const thName = ['Address', 'Map', 'Open','Close', 'Card', 'Capacity' ,'Smoke', 'Parking', 'Hot Pepper']
+
+            //trタグを生成して表示項目を追tdに追加
             for(let i = 0; i < shopParam.length; i++){
-                const p = document.createElement('p');
-                if(i == 3){
+                const tr = document.createElement('tr');
+                const th = document.createElement('th');
+                const td = document.createElement('td');
+                th.textContent = thName[i];
+                tr.appendChild(th);
+                if(i == 1 || i == 8){
                     const a = document.createElement('a');
                     a.href = shopParam[i];
-                    a.textContent = 'ネット予約はこちらから';
-                    p.appendChild(a);
+                    if(i == 1){
+                        a.textContent = '地図へ';
+                    }
+                    else if(i == 8){
+                        a.textContent = 'Hottpepperへ';
+                    }
+                    td.appendChild(a);
                 }else{
-                    p.textContent = shopParam[i];
+                    td.textContent = shopParam[i];
                 }
-                section.appendChild(p);
+                tr.appendChild(td);
+                table.appendChild(tr);
             }
-            
+            div_main.appendChild(table);
+            section.appendChild(div_main);
         }
-
         
     }, 
     (error) => {
--- tpl/shop_v.php
+++ tpl/shop_v.php
@@ -4,16 +4,23 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- <link rel="stylesheet" href="style.css"> -->
+<link rel="stylesheet" href="css/destyle.css">
+<link rel="stylesheet" href="css/header.css">
+<link rel="stylesheet" href="css/shop.css">
 <title>店舗詳細</title>
 </head>
 <body>
 <header>
-    <h1>店舗詳細</h1>
+    <h1>My Favorite Restaurant</h1>
 </header>
 <main>
     <section id="shop_data"></section>
-    <p><a href="./">戻る</a></p>
+    
+    <div id="menu">
+        <ul>
+            <li><a href="./">戻る</a></li>
+        </ul>
+    </div>
 </main>
 <fotter><fotter>
 <script type="module" src="./js/shop.js"></script>
