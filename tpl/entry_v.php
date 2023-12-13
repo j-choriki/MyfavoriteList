a9cda3dd01d499932de9cf7938f72453169d1cdb
j-choriki <102720007+j-choriki@users.noreply.github.com>
2023-12-13 10:51:43 +0900

    デザイン終了

--- tpl/entry_v.php
+++ tpl/entry_v.php
@@ -5,7 +5,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="./css/destyle.css">
-<!-- <link rel="stylesheet" href="./css/style.css"> -->
+<link rel="stylesheet" href="./css/style.css">
+<link rel="stylesheet" href="./css/entry.css">
 <title>新規登録</title>
 </head>
 <body>
@@ -18,22 +19,25 @@
         <h1>会員登録</h1>
         <div>
             <label for="mail">メールアドレス<span><?php echo $mail_msg;?></span></label>
-            <input type="text" name="mail">
+            <input type="text" name="mail" id="mail" value="">
         </div>
 
         <div>
             <label for="pass">パスワード<span><?php echo $pass_msg;?></span></label>
-            <input type="pass" name="pass" placeholder="4~10文字以内の英数字で入力"> 
+            <input type="password" name="pass" placeholder="4~10文字以内の英数字で入力" id="pass"> 
         </div>
-        <button name="btn" value="conf">確認</button>
+        <div id="btn">
+            <p><a href="./index.php">戻る</a></p>
+            <p><a href="./login.php">ログイン画面</a></p>
+            <button name="btn" value="conf">登録</button>
+        </div>
     </form>
-    <p><a href="./login.php">ログイン画面</a></p>
-    <p><a href="./index.php">戻る</a></p>
+
 </main>
 
 <footer>
+    <p>Powered by <a href="http://webservice.recruit.co.jp/">ホットペッパーグルメ Webサービス</a></p>
 </footer>
-
 
 <!-- <script></script> -->
 </body>
--- tpl/login_v.php
+++ tpl/login_v.php
@@ -5,7 +5,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="./css/destyle.css">
-<!-- <link rel="stylesheet" href="./css/style.css"> -->
+<link rel="stylesheet" href="./css/style.css">
+<link rel="stylesheet" href="./css/login.css">
 <title>ログイン画面</title>
 </head>
 <body>
@@ -15,24 +16,27 @@
 <main>
     <form action="./login.php" method="post">
         <h1>ログイン</h1>
-        <p><?php echo $msg;?></p>
+        <p id="err_msg"><?php echo $msg;?></p>
         <div>
             <label for="mail">メールアドレス</label>
             <input type="text" name="mail" value="" id="mail">
         </div>
         <div>
             <label for="pass">パスワード</label>
-            <input type="pass" name="pass" id="pass">
+            <input type="password" name="pass" id="pass">
         </div>
-        <button name="btn" value="login">ログイン</button>
+        <div id="btn">
+            <p><a href="./index.php">戻る</a></p>
+            <button name="btn" value="login">ログイン</button>
+        </div>
     </form>
     <p><a href="./entry.php">新規登録</a>
     <p><a href="./index.php">ホーム</a></p>
 </main>
-
 <footer>
-
+    <p>Powered by <a href="http://webservice.recruit.co.jp/">ホットペッパーグルメ Webサービス</a></p>
 </footer>
+
 <!-- <script></script> -->
 </body>
 </html>
